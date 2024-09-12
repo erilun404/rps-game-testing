5e08245f5810639d066f30ec1920335bf07eb3fc
Maria Hendricks <maria.hendricks26@gmail.com>
2024-09-12 15:47:16 +0200

    added content in selection panel

--- src/components/SelectionPanel/index.tsx
+++ src/components/SelectionPanel/index.tsx
@@ -1,17 +1,14 @@
-type selectionPanelProps = {
-    onSelect: (choice: string) => void;
-  }
-
-const SelectionPanel = ({onSelect}:selectionPanelProps) => {
- 
-
-    return (
-        <div>
-            <button onClick={() => onSelect('Rock')}>Rock</button> 
-            <button>Paper</button>
-            <button>Scissors</button>
-        </div>
-    )
-}
-
-export default SelectionPanel
+'use client';
+type SelectionPanelProps = {
+  onSelect: (choice: string) => void;
+};
+const SelectionPanel = ({ onSelect }: SelectionPanelProps) => {
+  return (
+    <div>
+      <button onClick={() => onSelect("Rock")}>Rock</button>
+      <button onClick={() => onSelect("Paper")}>Paper</button>
+      <button onClick={() => onSelect("Scissors")}>Scissors</button>
+    </div>
+  );
+};
+export default SelectionPanel;
