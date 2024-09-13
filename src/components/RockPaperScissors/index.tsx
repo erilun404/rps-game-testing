f82d3139605152037a283c088be36055033a2cd6
Ewlanna <erika.lundstrom@student.arenakoncernen.se>
2024-09-13 11:25:47 +0200

    choose name displayed and working

--- src/components/RockPaperScissors/index.tsx
+++ src/components/RockPaperScissors/index.tsx
@@ -1,19 +1,25 @@
 'use client'
 import SelectionPanel from "../SelectionPanel"
 import { useState } from "react"
-
+import ChooseName from "../ChooseName"
 
 const RockPaperScissors = () => {
     const [choice, setChoice] = useState('')
-    
+    const [name, setName] = useState('')
     
     const selectedChoice = (choice:string) => {
         setChoice(choice)
         console.log(choice)
     }
+    const userName = (name:string) => {
+        setName(name)
+        console.log(name)
+    }
     return (
         <>
+        <ChooseName updateName={userName} />
         <SelectionPanel onSelect={selectedChoice} />
+        
         </>
         
     )
